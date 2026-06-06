a5b759498112008c5756629eb3de16b92560c322
Kartikeya Bhatnagar <bhanagarkartikeya@gmail.com>
2026-06-06 14:41:15 +0530

    Remove github startup dependency

--- backend/rag/repo_registry.py
+++ backend/rag/repo_registry.py
@@ -1,17 +1,26 @@
-from backend.rag.github_ingestor import GitHubIngestor
-
-
 class RepoRegistry:
 
     def __init__(self):
 
-        github = GitHubIngestor("krtkay")
-
-        repos = github.get_repositories()
-
         self.repo_names = [
-            repo["name"]
-            for repo in repos
+
+            "Bank-Management-System",
+            "DEEP-LEARNING-MODELS-20-INDIAN-STOCKS-DATA-LSTM-CNN-GRU-RNN",
+            "Explorer-X",
+            "html-portfolio",
+            "HUGGING-FACE-GEMINI-OPENCV-SKETCH-TO-IMAGE-GENERATION",
+            "kaathi_",
+            "krtkay",
+            "leetcode",
+            "myresume",
+            "plant_disease",
+            "prettymapp",
+            "reelVoyage",
+            "voteEase",
+            "vote_ease",
+            "vulgarVeto",
+            "kartikeya-persona"
+
         ]
 
     def get_repo_names(self):
