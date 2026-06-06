4f3286905a15588dc2b919cd8ad16a76fbf8ed17
Kartikeya Bhatnagar <bhanagarkartikeya@gmail.com>
2026-06-06 17:19:32 +0530

    auto create scheduler tables

--- main.py
+++ main.py
@@ -4,9 +4,20 @@ from backend.api.chat import router as chat_router
 from backend.api.scheduling import router as scheduling_router
 from backend.api.vapi import router as vapi_router
 
+from backend.scheduler.models import (
+    create_tables
+)
+
 app = FastAPI(
     title="Kartikeya AI Persona"
 )
+
+
+@app.on_event("startup")
+def startup():
+
+    create_tables()
+
 
 app.include_router(chat_router)
 app.include_router(scheduling_router)
